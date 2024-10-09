7f2939fb7da63bc3875f2cb691368d54a3c3b5a3
Pardeep-Chaurasiya <chaurasiyapardeep001@gmail.com>
2024-10-09 11:25:57 +0530

    sidebar 7:10

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import AllAppointment from './pages/Admin/AllAppointment';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorList from './pages/Admin/DoctorList';
+import Dashboard from './pages/Admin/Dashboard';
 
 const App = () => {
 
@@ -13,6 +19,17 @@ const App = () => {
     <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
       <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/>}/>
+          <Route path='/all-appointment' element={<AllAppointment/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='/doctor-list' element={<DoctorList/>}/>
+
+        </Routes>
+      </div>
     </div>
   ):(
     <div>
