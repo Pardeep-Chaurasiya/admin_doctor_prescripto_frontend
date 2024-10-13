dc31e84cfb3604ca407136fcfda73deba28c5547
Pardeep-Chaurasiya <chaurasiyapardeep001@gmail.com>
2024-10-13 18:09:08 +0530

    change availibility api 8:30

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,44 +1,65 @@
 import { createContext, useState } from "react";
 import axios from "axios";
-import {toast} from "react-toastify"
+import { toast } from "react-toastify";
 
-export const AdminContext = createContext()
+export const AdminContext = createContext();
 
-const AdminContextProvider = (props) =>{
+const AdminContextProvider = (props) => {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+  const [doctors, setDoctors] = useState([]);
 
-    const  [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'')
-    const [doctors,setDoctors] = useState([])
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-
-    const getAllDoctors = async()=>{
-        try{
-            const data = await axios.get(`${backendUrl}/api/admin/all-doctors`, {
-                headers: { token }
-            });
-            if(data.data.success){
-                console.log(data.data.doctors)
-                setDoctors(data.data.doctors)
-            }else{
-                toast.error(data.message)
-            }
-        }
-        catch(err){
-            toast.error(err.message)
+  const getAllDoctors = async () => {
+    try {
+      const data = await axios.get(`${backendUrl}/api/admin/all-doctors`, {
+        headers: { token },
+      });
+      if (data.data.success) {
+        // toast.success(data.data.message);
+        setDoctors(data.data.doctors);
+      } else {
+        toast.error(data.data.message);
+      }
+    } catch (err) {
+      toast.error(err.message);
     }
+  };
+
+  const changeAvailability = async (docId) => {
+    try {
+      const data = await axios.patch(
+        `${backendUrl}/api/admin/change-availability`,
+        { docId },
+        { headers: { token } }
+      );
+      if (data.data.success) {
+        toast.success(data.data.message);
+        getAllDoctors();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      toast.error(err.message);
     }
+  };
 
+  const value = {
+    token,
+    setToken,
+    backendUrl,
+    doctors,
+    getAllDoctors,
+    changeAvailability,
+  };
 
-    const value={
-        token,setToken,
-        backendUrl,doctors,getAllDoctors
-    }
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
 
-    return(
-        <AdminContext.Provider value={value}>
-            {props.children}
-        </AdminContext.Provider>
-    )
-}
-
-export default AdminContextProvider
+export default AdminContextProvider;
