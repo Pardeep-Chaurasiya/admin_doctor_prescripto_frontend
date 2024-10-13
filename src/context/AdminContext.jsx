a2e05394d27d723ee1c17ac2aa3cea9077495030
Pardeep-Chaurasiya <chaurasiyapardeep001@gmail.com>
2024-10-13 17:46:59 +0530

    all doctor 8:19

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,17 +1,37 @@
 import { createContext, useState } from "react";
+import axios from "axios";
+import {toast} from "react-toastify"
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) =>{
 
     const  [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'')
+    const [doctors,setDoctors] = useState([])
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const getAllDoctors = async()=>{
+        try{
+            const data = await axios.get(`${backendUrl}/api/admin/all-doctors`, {
+                headers: { token }
+            });
+            if(data.data.success){
+                console.log(data.data.doctors)
+                setDoctors(data.data.doctors)
+            }else{
+                toast.error(data.message)
+            }
+        }
+        catch(err){
+            toast.error(err.message)
+    }
+    }
 
 
     const value={
         token,setToken,
-        backendUrl,
+        backendUrl,doctors,getAllDoctors
     }
 
     return(
